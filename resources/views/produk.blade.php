65fe3c699b5e91a97696aa953b0e2dd15e52586a
robi20agts <robby.surian03@gmail.com>
2023-11-17 20:11:42 +0700

    UTS Laravel

--- resources/views/produk.blade.php
+++ resources/views/produk.blade.php
@@ -4,9 +4,10 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Halaman | Home</title>
+    <title>Halaman | Produk</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
@@ -39,10 +40,44 @@
                 </div>
             </div>
         </div>
-        <div class="container">
-            <h1>Halaman Home</h1>
-        </div>
     </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg">
+                <h1 class="judul-produk">Halaman Produk</h1>
+            </div>
+            <div class="col-lg">
+                <div class="d-flex justify-content-end tombol-tambah">
+                    <a class="btn btn-primary" href="{{ url('/form') }}" role="button">Tambah Produk</a>
+                </div>
+            </div>
+        </div>
+
+        <table class="table table-bordered table-primary">
+            <thead>
+                <tr>
+                    <th>Kode Produk</th>
+                    <th>Nama Produk</th>
+                    <th>Jenis Produk</th>
+                    <th>Harga</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php
+                    $nilai_awal = 0;
+                @endphp
+                @for ($i = 0; $i < $jumlah; $i++)
+                    <tr>
+                        <td>{{ $kode_produk[$i] }}</td>
+                        <td>{{ $nama_produk[$i] }}</td>
+                        <td>{{ $jenis_produk[$i] }}</td>
+                        <td>{{ $harga[$i] }}</td>
+                    </tr>
+                @endfor
+            </tbody>
+        </table>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
